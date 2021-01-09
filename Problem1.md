83c1777a718735f535e03faa0b636bdec2d91d0c
bitinbyte <bitinbyte@outlook.com>
2021-01-09 22:07:56 +0000

    Problem 1 Solved

---
+++ Problem1.md
@@ -0,0 +1,2 @@
+# Problem 1
+To Solve this problem, python the module operator (%) which gives us the reminder number of a division, with that, was only needed to divide each number by 3 and by 5, check if the reminder is 0 and then add each number that have 0 reminder to a variable and that's it. 
---
+++ Problem1.py
@@ -0,0 +1,23 @@
+import sys
+
+# Check if the user inserted any agument
+if len(sys.argv) > 1:
+	# Get the max number as argument
+	maxNumber = int(sys.argv[1])
+	# Aux loop
+	i = 1
+	# Variable to get the result
+	total = 0
+	# Loop through each number until reaches maxNumber
+	while i < maxNumber:
+		# If is divisible by 3 or 5
+		if i % 3 == 0 or i % 5 == 0:
+			# Add the number to the total
+			total += i
+		# Iterate 1 more
+		i += 1
+	# Ouput the result
+	print(total)
+else:
+	# Output an error if the user didn't inserted any argument
+	print("No max value was introduced as argument!")
